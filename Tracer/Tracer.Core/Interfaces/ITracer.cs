5d99e1036c1df85cf2b986ada2132663639daf7b
Stalbar <aleksey.smorshyok@gmail.com>
2022-09-13 09:48:52 +0300

    add Tracer.Core

---
+++ Tracer/Tracer.Core/Interfaces/ITracer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer.Core.Structs;
+
+namespace Tracer.Core.Interfaces
+{
+    public interface ITracer
+    {
+        void StartTrace();
+        void StopTrace();
+        TraceResult GetTraceResult();
+    }
+}
---
+++ Tracer/Tracer.Core/Structs/TraceResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Core.Structs
+{
+    public struct TraceResult
+    {
+        public List<string> MethodNames;
+        public List<string> ClassNames;
+        public List<string> ParentMethodsNames;
+        public List<long> ElapsedTimeInMilliseconds;
+        public List<int> CurrentThreadsIds;
+
+        public TraceResult()
+        {
+            MethodNames = new List<string>();
+            ClassNames = new List<string>();
+            ParentMethodsNames = new List<string>();
+            ElapsedTimeInMilliseconds = new List<long>();
+            CurrentThreadsIds = new List<int>();
+        }
+
+        public void AddResult(string methodName, string className, string parentMethodName, long elapsedMilliseconds, int currentThreadId)
+        {
+            MethodNames.Add(methodName);
+            ClassNames.Add(className);
+            ParentMethodsNames.Add(parentMethodName);
+            ElapsedTimeInMilliseconds.Add(elapsedMilliseconds);
+            CurrentThreadsIds.Add(currentThreadId);
+        }
+    }
+}
