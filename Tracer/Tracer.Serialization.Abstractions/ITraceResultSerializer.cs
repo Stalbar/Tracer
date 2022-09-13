ffdfe38b0396d862507a9b3a15a38d8947becc65
Stalbar <aleksey.smorshyok@gmail.com>
2022-09-13 09:55:06 +0300

    Add ITraceResultSerializer interface

---
+++ Tracer/Tracer.Serialization.Abstractions/ITraceResultSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer.Serialization.Abstractions.Classes;
+
+namespace Tracer.Serialization.Abstractions
+{
+    public interface ITraceResultsSerializer
+    {
+        void Serialize(List<ThreadInfo> results, Stream to);
+    }
+}
