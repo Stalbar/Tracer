57b97090378d8619601864cf5324757851b903f0
Stalbar <aleksey.smorshyok@gmail.com>
2022-09-13 10:07:59 +0300

    create JSON serialization

---
+++ Tracer.Serialization/Tracer.Serialization.Json/JsonTraceResultSerializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Serialization.Json
+{
+    public class JsonTraceResultsSerializer : ITraceResultsSerializer
+    {
+        public void Serialize(List<ThreadInfo> threads, Stream to)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine("\t\"threads\": [");
+            foreach (var thread in threads)
+            {
+                sb.AppendLine("\t\t{");
+                sb.AppendLine($"\t\t\t\"id\": \"{thread.Id}\"");
+                sb.AppendLine($"\t\t\t\"time\": \"{thread.Time}ms\"");
+                sb.AppendLine($"\t\t\t\"methods\": [");
+                sb.Append(SerializeMethods(thread.Methods, "\t\t\t\t"));
+                sb.AppendLine("\t\t\t]");
+                sb.AppendLine("\t\t},");
+            }
+            sb.AppendLine("\t]");
+            sb.AppendLine("}");
+            using (StreamWriter writer = new StreamWriter(to))
+            {
+                writer.Write(sb);
+            }
+        }
+
+        private string SerializeMethods(List<MethodInfo> methods, string offset)
+        {
+            StringBuilder sb = new();
+            foreach (var method in methods)
+            {
+                sb.AppendLine($"{offset}{{");
+                sb.AppendLine($"{offset}\t\"name\": \"{method.Name}\"");
+                sb.AppendLine($"{offset}\t\"class\": \"{method.Class}\"");
+                sb.AppendLine($"{offset}\t\"time\": \"{method.Time}ms\"");
+                sb.AppendLine($"{offset}\t\"methods\": [");
+                sb.Append(SerializeMethods(method.Methods, offset + "\t"));
+                sb.AppendLine($"{offset}\t]");
+                sb.AppendLine($"{offset}}},");
+            }
+            return sb.ToString();
+        }
+    }
+}
--- Tracer.Serialization/Tracer.Serializaton.Yaml/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tracer.Serializaton.Yaml
-{
-    public class Class1
-    {
-
-    }
-}
